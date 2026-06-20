55ff034e8e50affd0bd40cf2850863063241cce2
dennis <machariad196@gmail.com>
2026-06-20 03:39:53 +0300

    made sure that the app allows log in using merchant table

--- laravel/config/auth.php
+++ laravel/config/auth.php
@@ -62,7 +62,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\Models\Merchant::class,  // Updated to use the Merchant model instead of the default User model
         ],
 
         // 'users' => [
