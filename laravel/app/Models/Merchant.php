ba5bf811750d2a8a345c0c000e5c7b95d71ef66c
dennis <machariad196@gmail.com>
2026-06-20 03:40:39 +0300

    updated the model to allow a user to log in with it

--- laravel/app/Models/Merchant.php
+++ laravel/app/Models/Merchant.php
@@ -2,22 +2,62 @@
 
 namespace App\Models;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens; // Required for Sanctum issueTokens/createToken
 
 class Merchant extends Authenticatable
 {
-    protected $fillable = ['business_name', 'email', 'password'];
+    use HasApiTokens, HasFactory, Notifiable;
 
-    protected $hidden = ['password'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',          // Matches $request->name in your AuthController registration method
+        'business_name', 
+        'email', 
+        'password'
+    ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password'          => 'hashed', // Automatically handles Hash::make() on save/create
+    ];
+
+    /**
+     * Get the products owned by this merchant.
+     */
     public function products(): HasMany
     {
-        return $this->hasMany(Product::class);
+        // Explicitly defining 'merchant_id' matching your product schema setup
+        return $this->hasMany(Product::class, 'merchant_id');
     }
 
+    /**
+     * Get the orders belonging to this merchant.
+     */
     public function orders(): HasMany
     {
-        return $this->hasMany(MerchantOrder::class);
+        return $this->hasMany(MerchantOrder::class, 'merchant_id');
     }
 }
